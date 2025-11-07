8fc1b48ee0f8ec5c01212b3e733fe1ac64ff8a41
Carsten Anand <email.carsten.anand@gmail.com>
2025-11-07 14:11:58 +0800

    Initial Commit

---
+++ Challenge 3 6AM/Challenge_3_6AMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_3_6AMApp.swift
+//  Challenge 3 6AM
+//
+//  Created by Carsten Anand on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_3_6AMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
