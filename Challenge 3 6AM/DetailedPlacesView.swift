ac1e548fbd855bb0624afa0ab338395174053252
Carsten Anand <email.carsten.anand@gmail.com>
2025-11-14 13:12:15 +0800

    changed the view name and added image

---
+++ Challenge 3 6AM/DetailedPlacesView.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftUIView.swift
+//  Challenge 3 6AM
+//
+//  Created by Carsten Anand on 10/11/25.
+//
+
+import SwiftUI
+
+
+struct DetailedPlacesView: View {
+    var location:String
+    
+    var body: some View {
+        VStack{
+            VStack(){
+            }
+            .navigationTitle(location)
+            Image("sample photo")
+                .resizable()
+                .scaledToFit()
+        }
+    }
+}
+
+#Preview {
+    NavigationStack{
+        DetailedPlacesView(location:"@solaris")
+    }
+}
