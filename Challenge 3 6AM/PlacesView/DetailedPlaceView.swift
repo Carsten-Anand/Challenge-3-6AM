7e6e4da343b010f29efecbdab061aaa4d8920ca1
sin <syu3887682@gmail.com>
2025-11-14 12:53:52 +0800

    added dynamic list for placesview and added detailedpalceview

---
+++ Challenge 3 6AM/PlacesView/DetailedPlaceView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedPlaceView.swift
+//  Challenge 3 6AM
+//
+//  Created by T Krobot on 14/11/25.
+//
+
+import SwiftUI
+
+struct DetailedPlaceView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DetailedPlaceView()
+}
