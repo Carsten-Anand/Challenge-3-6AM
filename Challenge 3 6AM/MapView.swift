76bfe7e6ed10136606dac63c12082d1807e8ab42
pagejenna <jwjx8888@gmail.com>
2025-11-14 16:13:33 +0800

    pins can change coloursa accordingly

--- Challenge 3 6AM/MapView.swift
+++ Challenge 3 6AM/MapView.swift
@@ -7,107 +7,93 @@
 
 
 //todo
-//change colour of pins
-//make pins interactive
+//when pin is clicked it should give moreinfosheet the name and it opens the place
 
 import SwiftUI
 import MapKit
 import UIKit
 
 enum RegionOptions{
-    case North
-    case Northeast
-    case Central
-    case West
-    case East
+    case north
+    case northeast
+    case central
+    case west
+    case east
 }
 
-struct Place: Identifiable {
+extension CLLocationCoordinate2D: @retroactive Equatable {}
+extension CLLocationCoordinate2D: @retroactive Hashable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude)
+        hasher.combine(longitude)
+    }
+}
+
+struct Place: Identifiable, Equatable, Hashable {
+    static func == (lhs: Place, rhs: Place) -> Bool {
+        lhs.coordinates == rhs.coordinates
+    }
+    
     let id = UUID()
     let name: String
     let coordinates: CLLocationCoordinate2D
     var region: RegionOptions
     let description: String
+    let markerTint: UIColor
 }
 
 struct MapView: View {
-    @State private var area = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 1.3521_051, longitude: 103.822872), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 1.3521_051, longitude: 103.822872), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)))
+    
+    @State private var place: Place? = nil
     
     let annotations = [
-        Place(name: "bukit timah", coordinates: CLLocationCoordinate2D(latitude: 1.3548, longitude: 103.7763), region: .Central, description: "Bukit Timah is one of Singapore’s greenest and most historic districts, best known for Bukit Timah Nature Reserve and the island’s highest hill. It offers rich biodiversity, forest trails, and challenging hikes for outdoor lovers. Beyond nature, the area features the Rail Corridor, charming cafés, and local eateries around Beauty World. Visitors can explore heritage spots, enjoy scenic viewpoints, cycle nearby park connectors, or unwind in quiet green spaces. " ),
-        Place(name: "mbs", coordinates: CLLocationCoordinate2D(latitude: 1.2838, longitude: 103.8591), region: .Central, description: "Marina Bay Sands (MBS) is an iconic integrated resort known for its striking architecture and luxury experiences. Visitors can enjoy the SkyPark Observation Deck with panoramic city views, shop at high-end boutiques, dine at celebrity restaurants, and explore ArtScience Museum. At night, the waterfront area comes alive with light shows and vibrant city scenery."),
-        Place(name: "woodlands stadium", coordinates: CLLocationCoordinate2D(latitude: 1.4352509353227207, longitude: 103.78029123839492), region: .North, description: "Woodlands Stadium is a community sports venue in the north of Singapore, known for its running track, football field, and open, spacious surroundings. Visitors can jog, train, or play field sports, and the nearby Sports Hall and swimming complex offer more activities.")
+        Place(name: "bukit timah", coordinates: CLLocationCoordinate2D(latitude: 1.3548, longitude: 103.7763), region: .central, description: "Bukit Timah is one of Singapore’s greenest and most historic districts, best known for Bukit Timah Nature Reserve and the island’s highest hill. It offers rich biodiversity, forest trails, and challenging hikes for outdoor lovers. Beyond nature, the area features the Rail Corridor, charming cafés, and local eateries around Beauty World. Visitors can explore heritage spots, enjoy scenic viewpoints, cycle nearby park connectors, or unwind in quiet green spaces. ", markerTint: .yellow),
+        Place(name: "mbs", coordinates: CLLocationCoordinate2D(latitude: 1.2838, longitude: 103.8591), region: .central, description: "Marina Bay Sands (MBS) is an iconic integrated resort known for its striking architecture and luxury experiences. Visitors can enjoy the SkyPark Observation Deck with panoramic city views, shop at high-end boutiques, dine at celebrity restaurants, and explore ArtScience Museum. At night, the waterfront area comes alive with light shows and vibrant city scenery.", markerTint: .green),
+        Place(name: "woodlands stadium", coordinates: CLLocationCoordinate2D(latitude: 1.4352509353227207, longitude: 103.78029123839492), region: .north, description: "Woodlands Stadium is a community sports venue in the north of Singapore, known for its running track, football field, and open, spacious surroundings. Visitors can jog, train, or play field sports, and the nearby Sports Hall and swimming complex offer more activities.", markerTint: .blue)
     ]
     
     
     var body: some View {
         
         ZStack{
-            Map(coordinateRegion: $area, annotationItems: annotations) {
-                MapPin(coordinate: $0.coordinates, )
+            Map(position: $position, selection: $place) {
+                ForEach(annotations){
+                    Marker($0.name, coordinate: $0.coordinates)
+                        .tint(Color($0.markerTint))
+                        .tag($0)
+                }
+            }
+            .ignoresSafeArea()
+            .onChange(of: place) { oldValue, newValue in
+                print(newValue)
             }
         }
         .overlay(alignment: .topLeading){
-            VStack{
-//                Spacer()
-                HStack{
-//                    Spacer()
-                    VStack(alignment: .leading){
-                        Text("Legend")
-                            .padding()
-                            .font(.title)
-                        Label("Suggested", systemImage: "dot.circle.fill")
-                            .font(.headline)
-                            .foregroundStyle(.blue)
-                            .padding()
-                        Label("Interested", systemImage: "dot.circle.fill")
-                            .font(.headline)
-                            .foregroundStyle(.yellow)
-                            .padding()
-                        
-                    }
-                 //   .background(.ultraThinMaterial)
-                    .opacity(0.7)
-                    .cornerRadius(20)
-                    .shadow(radius: 10)
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-//                    Spacer()
-                }
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
-//                Spacer()
+            VStack(alignment: .leading){
+                Text("Legend")
+                    .padding()
+                    .font(.headline)
+                Label("Suggested", systemImage: "dot.circle.fill")
+                    .font(.callout)
+                    .foregroundStyle(.blue)
+                    .padding()
+                Label("Interested", systemImage: "dot.circle.fill")
+                    .font(.headline)
+                    .foregroundStyle(.yellow)
+                    .padding()
+                
             }
-            
-            
-           
-            
+            .padding(3)
+            .shadow(radius: 10)
+            .glassEffect(.clear, in: .rect)
+            .cornerRadius(25)
+            .glassEffectTransition(.materialize)
         }
-        
     }
 }
 
