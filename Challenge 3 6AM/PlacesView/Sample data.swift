aaaf65f639a4553e85e857fb669f58c490dddf65
Carsten Anand <email.carsten.anand@gmail.com>
2025-11-14 16:24:41 +0800

    fixed place struct

--- Challenge 3 6AM/PlacesView/Sample data.swift
+++ Challenge 3 6AM/PlacesView/Sample data.swift
@@ -5,13 +5,18 @@
 //  Created by Carsten Anand on 14/11/25.
 //
 
-
-//struct Place: Identifiable {
+//struct Place: Identifiable, Equatable, Hashable {
+//    static func == (lhs: Place, rhs: Place) -> Bool {
+//        lhs.coordinates == rhs.coordinates
+//    }
+//    
 //    let id = UUID()
 //    let name: String
 //    let coordinates: CLLocationCoordinate2D
 //    var region: RegionOptions
 //    let description: String
+//    let markerTint: UIColor
+//}
 import MapKit
 
-let sampleData = Place(name: "Solaris", coordinates: CLLocationCoordinate2D(latitude: 1.3548, longitude: 103.7763), region: .East, description: "learning coding", status: .Suggested)
+let sampleData = Place(name: "Solaris", coordinates: CLLocationCoordinate2D(latitude: 1.3548, longitude: 103.7763), region: .east, description: "learning coding",markerTint: .blue)
