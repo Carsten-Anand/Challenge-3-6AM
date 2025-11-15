691c84b64c079a261c45c7772aa2eee7a275f30f
pagejenna <jwjx8888@gmail.com>
2025-11-15 10:15:27 +0800

    added sheet for map legend

---
+++ Challenge 3 6AM/LegendSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  LegendSheetView.swift
+//  Challenge 3 6AM
+//
+//  Created by jenna on 15/11/25.
+//
+
+import SwiftUI
+
+struct LegendSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LegendSheetView()
+}
