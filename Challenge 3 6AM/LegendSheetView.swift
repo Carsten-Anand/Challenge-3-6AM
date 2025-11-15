2e9af31ccb8d4d520bc5d14bcca596c28af1e301
pagejenna <jwjx8888@gmail.com>
2025-11-15 10:16:05 +0800

    added sheet for map legend

--- Challenge 3 6AM/LegendSheetView.swift
+++ Challenge 3 6AM/LegendSheetView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct LegendSheetView: View {
+    @Binding var showingLegendSheetView: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Spacer()
+            Text("‼️important‼️")
+                .font(.title)
+            
+            Text("Click on the map pins to see more information!")
+                .padding()
+            
+            Text("The pins highlighted in blue are suggested places")
+                .foregroundStyle(Color.blue)
+                .padding()
+            
+            Text("The pins highlighted in yellow are places that you have indicated interest in")
+                .foregroundStyle(Color.yellow)
+                .padding()
+        }
+        
+        Spacer()
+        Button("Dismiss"){
+            showingLegendSheetView = false
+        }
     }
 }
 
-#Preview {
-    LegendSheetView()
-}
+//#Preview {
+//    LegendSheetView()
+//}
