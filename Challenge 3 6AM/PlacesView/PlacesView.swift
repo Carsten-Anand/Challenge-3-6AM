0647f85bc34b51d851146dba3ea90fdc6c8bafd5
Carsten Anand <email.carsten.anand@gmail.com>
2025-11-17 14:41:57 +0800

    added picker and stuff

--- Challenge 3 6AM/PlacesView/PlacesView.swift
+++ Challenge 3 6AM/PlacesView/PlacesView.swift
@@ -15,7 +15,10 @@ struct PlacesView: View {
     @State private var places = convertCSVIntoArray()
     @Binding var displayedPlaces: [Place]
     @Binding var showingPlacesView: Bool
-
+    @State private var colourFilteredPlaces = "All"
+    var filterOptions = ["All", "Visited", "Reccomended", "Saved"]
+    
+    
     var filteredPlaces: [Place] {
         if searchText.isEmpty {
             return displayedPlaces
@@ -23,38 +26,47 @@ struct PlacesView: View {
             return displayedPlaces.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
         }
     }
-
+    
     func refreshPlaces() {
         displayedPlaces = Array(places.shuffled().prefix(15))
     }
-
+    
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(filteredPlaces) { place in
-                    NavigationLink(destination: DetailedPlacesView(data: place)) {
-                        Text(place.name)
+            VStack{
+                Picker("Filter", selection: $colourFilteredPlaces) {
+                    ForEach(filterOptions, id: \.self) {
+                        Text($0)
                     }
                 }
-                .onDelete { indexSet in
-                    // delete from the displayed list (not the master CSV list)
-                    displayedPlaces.remove(atOffsets: indexSet)
-                }
-            }
-            .listRowSpacing(10.0)
-            .navigationTitle("Search for places")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        refreshPlaces()
-                    } label: {
-                        Image(systemName: "arrow.clockwise")
+                .pickerStyle(.segmented)
+                List {
+                    ForEach(filteredPlaces) { place in
+                        NavigationLink(destination: DetailedPlacesView(data: place)) {
+                            Text(place.name)
+                        }
                     }
-                    .buttonStyle(.glass)
+                    .onDelete { indexSet in
+                        // delete from the displayed list (not the master CSV list)
+                        displayedPlaces.remove(atOffsets: indexSet)
+                    }
                 }
+                .listRowSpacing(10.0)
+                .navigationTitle("Search for places")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            refreshPlaces()
+                        } label: {
+                            Image(systemName: "arrow.clockwise")
+                        }
+                        .buttonStyle(.glass)
+                    }
+                }
+                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
             }
-            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
-        }.interactiveDismissDisabled()
+        }
+        .interactiveDismissDisabled()
         .onAppear {
             if displayedPlaces.isEmpty {
                 refreshPlaces()
@@ -62,3 +74,4 @@ struct PlacesView: View {
         }
     }
 }
+
