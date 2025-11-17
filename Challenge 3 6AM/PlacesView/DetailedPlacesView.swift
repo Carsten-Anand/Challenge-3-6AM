59a677fcd469d1ff8d8d4d18baf533c22db579a1
sin <syu3887682@gmail.com>
2025-11-17 20:51:54 +0800

    fix mapview and detailedplacesview

--- Challenge 3 6AM/PlacesView/DetailedPlacesView.swift
+++ Challenge 3 6AM/PlacesView/DetailedPlacesView.swift
@@ -10,41 +10,35 @@ import SwiftData
 
 struct DetailedPlacesView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \Place.sortIndex) var places: [Place]
-//    @State var places = convertCSVIntoArray()
-    
     var data: Place
+    
     var body: some View {
-        VStack {
-            
-            VStack(){}
-                .navigationTitle(data.name)
-            
-            Text(data.location)
-            
-            Image("sample photo")
-                .resizable()
-                .scaledToFit()
-                .padding()
-            
-            Text(data.desc)
-            Button {
+        NavigationStack{
+            VStack {
                 
-            } label: {
-                Text("Status updater")
-                    .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
-//                    .background(Capsule().fill(Color.blue.opacity(0.4)))
+                Text(data.location)
                 
-            }.buttonStyle(.glass)
-            
-            
-
+                Image("sample photo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                Text(data.desc)
+                    .padding()
+                Button {
+                    
+                } label: {
+                    Text("Status updater")
+                        .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                    //                    .background(Capsule().fill(Color.blue.opacity(0.4)))
+                    
+                }.buttonStyle(.glass)
+                
+                
+            }.navigationTitle(data.name)
         }
     }
 }
-
 #Preview {
-    NavigationStack{
-        DetailedPlacesView(data: sampleData[0])
-    }
+    DetailedPlacesView(data: sampleData[0])
 }
