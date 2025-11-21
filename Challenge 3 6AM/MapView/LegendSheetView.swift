601af04171351bc1def30504847903873ddaf101
Carsten Anand <email.carsten.anand@gmail.com>
2025-11-21 10:30:39 +0800

    added dismiss button

--- Challenge 3 6AM/MapView/LegendSheetView.swift
+++ Challenge 3 6AM/MapView/LegendSheetView.swift
@@ -10,39 +10,47 @@ import SwiftUI
 struct LegendSheetView: View {
     @Binding var showingLegendSheetView: Bool
     var body: some View {
-        VStack(alignment: .leading){
+        NavigationStack{
+            VStack(alignment: .leading){
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("‼️Important‼️")
+                    .font(.title)
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding()
+                
+                Text("Click on the map pins to see more information!")
+                    .padding()
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                Text("The pins highlighted in blue are suggested places")
+                    .foregroundStyle(Color.blue)
+                    .padding()
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                Text("The pins highlighted in yellow are places that you have indicated interest in")
+                    .foregroundStyle(Color.yellow)
+                    .padding()
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingLegendSheetView = false
+                    } label: {
+                        Image(systemName: "x.circle.fill")
+                    }
+                    .buttonStyle(.glass)
+                }
+            }
             Spacer()
-            Spacer()
-            Spacer()
-            Spacer()
-            Text("‼️important‼️")
-                .font(.title)
-                .lineLimit(nil)
-                .fixedSize(horizontal: false, vertical: true)
-            
-            Text("Click on the map pins to see more information!")
-                .padding()
-                .lineLimit(nil)
-                .fixedSize(horizontal: false, vertical: true)
-            
-            Text("The pins highlighted in blue are suggested places")
-                .foregroundStyle(Color.blue)
-                .padding()
-                .lineLimit(nil)
-                .fixedSize(horizontal: false, vertical: true)
-            
-            Text("The pins highlighted in yellow are places that you have indicated interest in")
-                .foregroundStyle(Color.yellow)
-                .padding()
-                .lineLimit(nil)
-                .fixedSize(horizontal: false, vertical: true)
         }
-        
-        Spacer()
-        Button("Dismiss"){
-            showingLegendSheetView = false
-        }.buttonStyle(.glass)
-        .background(Capsule().fill(Color.blue.opacity(0.4)))
     }
 }
 
